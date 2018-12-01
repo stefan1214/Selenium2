995e8266795869d9f49fd8ddae370d5a197a6dd2
StudentWSB <stefan1214@op.pl>
2018-12-01 09:24:09 +0100

    pierwszy commit uruchomienie strony filmweb

--- filmweb.py
+++ filmweb.py
@@ -1,3 +1,11 @@
-from config import driver
+from configFilmweb import driver
 
-driver.get("https://filmweb.pl")
+driver.get("https://www.filmweb.pl/")
+driver.find_element_by_xpath(
+    "(.//*[normalize-space(text()) and normalize-space(.)='REKLAMA:'])[1]/preceding::button[1]").click()
+driver.find_element_by_xpath(
+    u"(.//*[normalize-space(text()) and normalize-space(.)='Polityce prywatności'])[1]/following::button[1]").click()
+driver.find_element_by_id("inputSearch").click()
+driver.find_element_by_id("inputSearch").clear()
+driver.find_element_by_id("inputSearch").send_keys(u"tytuł filmu")
+driver.find_element_by_id("searchForm").submit()
