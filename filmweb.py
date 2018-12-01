be06b3aeadbed38381e009be9a88d3f0e69cafe9
StudentWSB <stefan1214@op.pl>
2018-12-01 12:56:01 +0100

    nazwa użytkownika

--- filmweb.py
+++ filmweb.py
@@ -1,10 +1,19 @@
 from configFilmweb import driver
 from filmwebUserConfig import filmWebUserName
+from selenium.common.exceptions import NoSuchElementException
 
 
 
-driver.get("https://www.filmweb.pl/user"+filmWebUserName+"/wantToSee")
+driver.get("https://www.filmweb.pl/user/"+filmWebUserName+"/wantToSee")
 
+try:
+    driver.find_element_by_id("goToLink").click()
+except NoSuchElementException:
+    print("Długa ta reklama"
+driver.find_element_by_xpath(
+    "(.//*[normalize-space(text()) and normalize-space(.)='REKLAMA:'])[1]/preceding::button[1]").click()
+driver.find_element_by_xpath(
+    u"(.//*[normalize-space(text()) and normalize-space(.)='Polityce prywatności'])[1]/following::button[1]").click()
 
 # driver.get("https://www.filmweb.pl/")
 # driver.find_element_by_xpath(
