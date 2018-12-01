3b49799522d484937d5fd35d561cd1077b167b9b
StudentWSB <stefan1214@op.pl>
2018-12-01 13:02:00 +0100

    nie commitować

--- filmwebUserConfig.py
+++ filmwebUserConfig.py
@@ -1,1 +1,12 @@
+
+#nazwa użytkownika którego lista fimów zostanie pobrana
 filmWebUserName="userName"
+
+#dane konta bota
+#bot musi być znajomym z uzytkownikiem którego lista filmów zostanie pobrana
+
+botUserName = "botUserName"
+botPassword = "botPassword"
+
+
+
