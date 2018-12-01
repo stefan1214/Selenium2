b08aca1068d1dcc8a0bf434d7686b03fde45118e
StudentWSB <stefan1214@op.pl>
2018-12-01 12:36:24 +0100

    nazwa użytkownika

--- filmweb.py
+++ filmweb.py
@@ -1,11 +1,17 @@
 from configFilmweb import driver
+from filmwebUserConfig import filmWebUserName
 
-driver.get("https://www.filmweb.pl/")
-driver.find_element_by_xpath(
-    "(.//*[normalize-space(text()) and normalize-space(.)='REKLAMA:'])[1]/preceding::button[1]").click()
-driver.find_element_by_xpath(
-    u"(.//*[normalize-space(text()) and normalize-space(.)='Polityce prywatności'])[1]/following::button[1]").click()
-driver.find_element_by_id("inputSearch").click()
-driver.find_element_by_id("inputSearch").clear()
-driver.find_element_by_id("inputSearch").send_keys(u"tytuł filmu")
-driver.find_element_by_id("searchForm").submit()
+
+
+driver.get("https://www.filmweb.pl/user"+filmWebUserName+"/wantToSee")
+
+
+# driver.get("https://www.filmweb.pl/")
+# driver.find_element_by_xpath(
+#     "(.//*[normalize-space(text()) and normalize-space(.)='REKLAMA:'])[1]/preceding::button[1]").click()
+# driver.find_element_by_xpath(
+#     u"(.//*[normalize-space(text()) and normalize-space(.)='Polityce prywatności'])[1]/following::button[1]").click()
+# driver.find_element_by_id("inputSearch").click()
+# driver.find_element_by_id("inputSearch").clear()
+# driver.find_element_by_id("inputSearch").send_keys(u"tytuł filmu")
+# driver.find_element_by_id("searchForm").submit()
---
+++ filmwebUserConfig.py
@@ -0,0 +1,1 @@
+filmWebUserName="userName"
